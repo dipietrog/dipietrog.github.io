d8021c6e3d95c9428b38dac30ea97f2afe7b55a5
dipietrog <127163306+dipietrog@users.noreply.github.com>
2023-03-08 18:13:20 -0300

    initial commit

---
+++ cv.js
@@ -0,0 +1,29 @@
+
+document.getElementById("botonocultar").addEventListener("click", function(){
+    document.getElementById("trabajo1").style.display ="none";
+    document.getElementById("trabajo2").style.display ="none";
+    document.getElementById("trabajo3").style.display ="none";
+    document.getElementById("trabajo4").style.display ="none";
+}) 
+
+document.getElementById("botonver").addEventListener("click", function(){
+    document.getElementById("trabajo1").style.display ="block";
+    document.getElementById("trabajo2").style.display ="list-item";
+    document.getElementById("trabajo3").style.display ="list-item";
+    document.getElementById("trabajo4").style.display ="list-item";
+}) 
+
+document.getElementById("botonocultar2").addEventListener("click", function(){
+    document.getElementById("trabajo5").style.display ="none";
+    document.getElementById("trabajo6").style.display ="none";
+    document.getElementById("trabajo7").style.display ="none";
+    document.getElementById("trabajo8").style.display ="none";
+}) 
+
+document.getElementById("botonver2").addEventListener("click", function(){
+    document.getElementById("trabajo5").style.display ="block";
+    document.getElementById("trabajo6").style.display ="list-item";
+    document.getElementById("trabajo7").style.display ="list-item";
+    document.getElementById("trabajo8").style.display ="list-item";
+}) 
+
